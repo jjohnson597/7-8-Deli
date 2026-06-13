e023905070429b4932ad8acb847073fdf4e3b9a9
Jaylen Johnson <jjohnson597@student.cscc.edu>
2026-06-13 14:07:29 -0400

    Process sandwich orders with a while loop.

--- Deli.py
+++ Deli.py
@@ -1,2 +1,7 @@
 sandwich_orders = ["salami", "blt", "ham and cheese", "peanut butter and jelly", "tuna"]
 finished_sandwiches = []
+
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop()
+    print(f"I made your {current_sandwich} sandwich.")
+    finished_sandwiches.append(current_sandwich)
