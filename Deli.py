c53f7d12de2d4388820cb93ba9d3c596a6840f0a
Jaylen Johnson <jjohnson597@student.cscc.edu>
2026-06-13 14:12:18 -0400

    Prints finished sandwich list

--- Deli.py
+++ Deli.py
@@ -5,3 +5,9 @@ while sandwich_orders:
     current_sandwich = sandwich_orders.pop()
     print(f"I made your {current_sandwich} sandwich.")
     finished_sandwiches.append(current_sandwich)
+
+print("\nThe following sandwiches were made:")
+
+for sandwich in finished_sandwiches:
+    print(f"- {sandwich}")
+    
